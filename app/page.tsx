f42c3e02b8b1070e11e08d486704f25a6c587274
1702london-maker <1702london@gmail.com>
2026-07-08 02:01:27 +0100

    Fix telephone to landline +44 (204) 652 3032 on home page

--- app/page.tsx
+++ app/page.tsx
@@ -198,14 +198,14 @@ export default function HomePage() {
               booking@budruum.co.uk
             </a>
             {" "}or call{" "}
-            <a href="tel:+447919643752" className="text-t1 font-medium border-b border-t1">
-              +44 7919 643 752
+            <a href="tel:+442046523032" className="text-t1 font-medium border-b border-t1">
+              +44 (204) 652 3032
             </a>
           </p>
           <div className="flex flex-wrap items-center gap-6">
             {[
               { href: "mailto:booking@budruum.co.uk", icon: <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>, extra: <polyline points="22,6 12,13 2,6"/>, label: "booking@budruum.co.uk" },
-              { href: "tel:+447919643752", icon: <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.38 2 2 0 0 1 3.6 1.21h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.96a16 16 0 0 0 6.13 6.13l1.27-.96a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/>, extra: null, label: "+44 7919 643 752" },
+              { href: "tel:+442046523032", icon: <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.61 3.38 2 2 0 0 1 3.6 1.21h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.96a16 16 0 0 0 6.13 6.13l1.27-.96a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/>, extra: null, label: "+44 (204) 652 3032" },
               { href: "https://wa.me/447919643752", icon: <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"/>, extra: null, label: "WhatsApp" },
             ].map((item, i) => (
               <a key={i} href={item.href} className="flex items-center gap-2 text-[13px] text-t2 hover:text-t1 transition-colors">
